0cd254750c0fcf4265e3cb3e99d229423471bd81
irfansofyana <irfansofyana0305@gmail.com>
2022-07-17 19:56:00 +0700

    feat: init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/irfansofyana/workcontributions
+
+go 1.17
---
+++ internal/domain/model/model.go
@@ -0,0 +1,31 @@
+package model
+
+// Work struct defines a work that a person does
+type Work struct {
+	What        string
+	RelatedLink string
+	Type        TypeOfWork
+}
+
+// TypeOfWork is custom type to hold value of type of work
+type TypeOfWork int
+
+const (
+	GithubPR TypeOfWork = iota
+	GithubIssue
+	JiraTicket
+)
+
+// String is a function to create common behavior - give the type a String function
+func (t TypeOfWork) String() string {
+	return [...]string{
+		"Github Pull Request",
+		"Github Issues",
+		"Jira Ticket",
+	}[t]
+}
+
+// NewWork is function to create new instance of Work
+func NewWork(what, relatedLink string, typeOfWork TypeOfWork) Work {
+	return Work{what, relatedLink, typeOfWork}
+}
